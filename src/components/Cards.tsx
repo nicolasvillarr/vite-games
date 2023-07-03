4f2aefb3c2815665358bf22cf6fc3c72b37e2d48
nicolasvillarr <nicolasvillar4@gmail.com>
2023-07-03 19:41:14 -0300

    changes in stlye cards

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -5,21 +5,28 @@ interface Props {
     games: Array<dataGames>
 }
 export default function Cards ({games}:Props) {
-    
-  return (
+      return (
     <div className="cont">
         {
             games.map(e => {
                 return(
                     <div className="cardConteiner">
                         <div className="Card">
-                        <h1 className="text-center">Titulo {e.title}</h1>
-                        <h3 className="text-center">Genero {e.genre}</h3>
-                        <img src={e.thumbnail} width={400} alt="" />
-                            <div>
-                                <Link to={`details/${e.id}`}>x</Link>
-                            </div>
-                        <h5>{e.short_description}</h5>
+                        <h1 className="text-center title">Titulo {e.title}</h1>
+                        <h3 className="text-center subTitle">Genero {e.genre}</h3>
+                        <div className="flex justify-center">
+                            <img className="" src={e.thumbnail} width={400} alt="" />
+                        </div>
+                        <div className="text-center">
+                        <Link  to={`details/${e.id}`}>Game details</Link>
+                        </div>
+                        <h5 className="text-center">{e.short_description}</h5>
+                        <h5 className="text-center">Platform {e.platform}</h5>
+                        <h5 className="text-center">publisher {e.publisher}</h5>
+                        <h5 className="text-center">developer {e.developer}</h5>
+                        <h5 className="text-center">release date {e.release_date}</h5>
+                        <a className="text-center" href={e.game_url} target="_blank">Download game here</a>
+                        
                             <div>add to fav⭐</div>
                         </div>
                     </div>
